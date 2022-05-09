f95c44b3cb4132fab9173a7046a5ced902f68657
Sepherd <78238871+Sepherd@users.noreply.github.com>
2022-05-09 18:53:42 +0200

    Add files via upload

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,67 @@
 /*   By: arecce <arecce@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 18:07:30 by arecce            #+#    #+#             */
-/*   Updated: 2022/04/14 18:07:31 by arecce           ###   ########.fr       */
+/*   Updated: 2022/05/09 15:28:34 by arecce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	neg;
+	int	num;
+	int	counter;
+
+	i = 0;
+	neg = 1;
+	num = 0;
+	counter = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	while ((str[i] == '-' || str[i] == '+') && counter++ == 0)
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	num *= neg;
+	return (num);
+}
+
+// void	msg_received(int x)
+// {
+// 	if (x == SIGUSR1)
+// 		write(1, "Messaggio ricevuto\n", 19);
+// 	return ;
+// }
+
+// void	msg_tosend(int sig, int pid)
+// {
+// 	printf("Messaggio inviato!");
+// 	kill(pid, SIGUSR1);
+// }
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc < 2)
+		return (printf("Error!\n"));
+	pid = ft_atoi(argv[1]);
+	// signal(SIGUSR1, msg_received);
+	if (argc == 2 && pid > 0)
+		kill(pid, SIGUSR1);
+	else
+		return (printf("Error PID.\n"));
+	return (0);
+}
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,28 @@
 /*   By: arecce <arecce@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 18:07:33 by arecce            #+#    #+#             */
-/*   Updated: 2022/04/14 18:07:34 by arecce           ###   ########.fr       */
+/*   Updated: 2022/05/09 18:37:31 by arecce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void	sig_handler(int sig)
+{
+	printf("Messaggio inviato!\n");
+}
+
+int	main(void)
+{
+	//int					pid;
+	struct sigaction	ft_sig;
+
+	ft_sig.sa_flags = SA_SIGINFO;
+	ft_sig.sa_handler = sig_handler;
+	//pid = getpid();
+	printf("PID: %d\nWaiting...\n", getpid());
+	sigaction(SIGUSR1, &ft_sig, NULL);
+	pause();
+}
